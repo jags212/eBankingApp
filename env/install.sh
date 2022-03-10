58b3564c2d81a0613e77c85399d9deff51cb7be2
Shashank Sharma <s21j-login@protonmail.com>
2022-03-10 21:41:58 +0530

    Update install.sh

--- env/install.sh
+++ env/install.sh
@@ -5,7 +5,9 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update; \
 sudo apt-get install -y apt-transport-https && \
 sudo apt-get update && \
-sudo apt-get install -y aspnetcore-runtime-5.0
-sudo apt-get install -y dotnet-sdk-5.0
-cd eBankingTests
-wget https://chromedriver.storage.googleapis.com/78.0.3904.105/chromedriver_linux64.zip && unzip chromedriver_linux64.zip && chmod 774 chromedriver
+sudo apt-get install -y aspnetcore-runtime-5.0 && \
+sudo apt-get install -y dotnet-sdk-5.0 && \
+cd eBankingTests && \
+wget https://chromedriver.storage.googleapis.com/78.0.3904.105/chromedriver_linux64.zip && \
+unzip chromedriver_linux64.zip && \
+chmod 774 chromedriver
