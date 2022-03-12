746eb500291066ee5ec9842ad19fdbcd8eeeceb3
Jagadish Mohanty <30613381+jags212@users.noreply.github.com>
2022-03-12 15:38:25 +0530

    Update install.sh

--- env/install.sh
+++ env/install.sh
@@ -7,5 +7,6 @@ sudo apt-get install -y apt-transport-https && \
 sudo apt-get update && \
 sudo apt-get install -y aspnetcore-runtime-5.0
 sudo apt-get install -y dotnet-sdk-5.0
-cd eBankingTests
+cd eBankingTests && \
+rm -rf chromedriver* && \
 wget https://chromedriver.storage.googleapis.com/78.0.3904.105/chromedriver_linux64.zip && unzip chromedriver_linux64.zip && chmod 774 chromedriver
