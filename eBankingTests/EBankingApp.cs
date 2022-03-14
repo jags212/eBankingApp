d89106c9f7e61c8b1af15ce4c964aa39948c7749
Jagadish Mohanty <30613381+jags212@users.noreply.github.com>
2022-03-14 19:33:51 +0530

    Update EBankingApp.cs

--- eBankingTests/EBankingApp.cs
+++ eBankingTests/EBankingApp.cs
@@ -1,4 +1,4 @@
-﻿using eBankingApp.Models;
+using eBankingApp.Models;
 using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
@@ -11,9 +11,18 @@ namespace eBankingTests
     public  class EBankingApp
     {
         public static int applyLoan(IWebDriver driver,
-            string fullyQualifiedUrlLoan)
+            string fullyQualifiedUrlLoan, string fullyQualifiedUrlSuccessLoan, LoanDetails loanDetails)
         {
             driver.Navigate().GoToUrl(fullyQualifiedUrlLoan);
+            
+            driver.FindElement(By.Id("fname")).SendKeys(loanDetails.firstName);
+            driver.FindElement(By.Id("lname")).SendKeys(loanDetails.lastName);
+            driver.FindElement(By.Id("email")).SendKeys(loanDetails.email);
+            driver.FindElement(By.Id("loanType")).SendKeys(loanDetails.loanType);
+            driver.FindElement(By.Id("loanDuration")).SendKeys(Convert.ToString(loanDetails.loanDuration));
+
+            driver.Navigate().GoToUrl(fullyQualifiedUrlSuccessLoan);
+
             var getHashCode = Convert.ToInt32(driver.FindElement(By.Id("hashCodeValue")).Text);
             return getHashCode;
         }
