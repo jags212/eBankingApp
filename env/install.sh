8e343bc7198bc76f1f8cca7adc1a86db4c7af5e8
Shashank Sharma <s21j-login@protonmail.com>
2022-03-15 11:28:24 +0530

    Update install.sh

--- env/install.sh
+++ env/install.sh
@@ -7,7 +7,15 @@ sudo apt-get install -y apt-transport-https && \
 sudo apt-get update && \
 sudo apt-get install -y aspnetcore-runtime-5.0 && \
 sudo apt-get install -y dotnet-sdk-5.0 && \
-cd eBankingTests && \
-wget https://chromedriver.storage.googleapis.com/78.0.3904.105/chromedriver_linux64.zip && \
-unzip -o chromedriver_linux64.zip && \
-chmod 774 chromedriver
+cd eBankingTests
+
+FILE=chromedriver
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist."
+    wget -O chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/78.0.3904.105/chromedriver_linux64.zip && \
+    unzip -o chromedriver_linux64.zip && \
+    chmod 774 chromedriver
+    rm chromedriver_linux64.zip
+else 
+    echo "$FILE exists, skipping download."
+fi
