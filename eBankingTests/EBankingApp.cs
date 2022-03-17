a25e5728f77f3d58d0402caeef2c9bf1c0dcb0fa
Jagadish Mohanty <30613381+jags212@users.noreply.github.com>
2022-03-17 13:15:15 +0530

    Update EBankingApp.cs

--- eBankingTests/EBankingApp.cs
+++ eBankingTests/EBankingApp.cs
@@ -13,30 +13,7 @@ namespace eBankingTests
          public static string applyLoan(IWebDriver driver,
             string fullyQualifiedUrlLoan,LoanDetails loanDetails)
         {
-            var getHashCode = string.Empty;
-            try
-            {
-                driver.Navigate().GoToUrl(fullyQualifiedUrlLoan);
-                driver.FindElement(By.Id("fname")).SendKeys(loanDetails.firstName);
-                driver.FindElement(By.Id("lname")).SendKeys(loanDetails.lastName);
-                driver.FindElement(By.Id("email")).SendKeys(loanDetails.email);
-
-                SelectElement oSelect = new SelectElement(driver.FindElement(By.Id("loanType")));
-                oSelect.SelectByValue(loanDetails.loanType);
-                oSelect.SelectByText(loanDetails.loanType);
-
-                driver.FindElement(By.Id("loanDuration")).SendKeys(Convert.ToString(loanDetails.loanDuration));
-
-                driver.FindElement(By.Id("submit")).Click();
-                getHashCode = driver.FindElement(By.Id("hashCodeValue")).Text;
-
-            }
-            catch (Exception ex)
-            {
-                getHashCode = ex.Message;
-            }
-            
-            return getHashCode;
+            return string.Empty;
         }
     }
 }
